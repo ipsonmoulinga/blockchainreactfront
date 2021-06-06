2e8dd91ef3fc6409064d1c7619010d5f3dbdb2d0
EliTheSurfer <elimane.sall@telecomnnancy.net>
2021-06-06 20:26:39 +0200

    alternative rendering

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,12 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React from 'react';
 import '../style/App.css';
-import DisplayBlockChain from './DisplayBlockChain';
+import DisplayBlockChain, { BlockChainComponent } from './DisplayBlockChain';
 
 function App() {
   return (
     <div>
+      <BlockChainComponent />
       Hello world !
       <DisplayBlockChain />
     </div>
--- src/components/DisplayBlockChain.tsx
+++ src/components/DisplayBlockChain.tsx
@@ -7,8 +7,31 @@ import { getBlockChain } from '../service/api';
 import { DisplayOneBlock } from './DisplayOneBlock';
 import { DisplayTransaction } from './DisplayTransaction';
 
+export class BlockChainComponent extends React.Component<any, { blockchain: any }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      blockchain: {},
+    };
+  }
+
+  async componentDidMount() {
+    const blockchain = await getBlockChain();
+    this.setState({ blockchain });
+  }
+
+  render() {
+    return (
+    <div>
+      <h1>BlockChain Difficulty : {this.state.blockchain.difficulty}</h1>
+      <h1>BlockChain miningReward : {this.state.blockchain.miningReward}</h1>
+    </div>
+    );
+  }
+}
+
 export const DisplayBlokChain = () => {
-  const emptyBlockChain:BlockChain = {
+  const emptyBlockChain: BlockChain = {
     chain: [], transactions: [], miningReward: 0, difficulty: 0,
   };
   const [blockchain, setBlockChain] = useState<BlockChain>(emptyBlockChain);
@@ -21,25 +44,25 @@ export const DisplayBlokChain = () => {
   }, []);
 
   return (
+    <div>
+      <p> Blockchain with a mining difficulty of {blockchain.difficulty} and a mining reward of {blockchain.miningReward}.</p>
+      <p>
+        Blocks :
+        </p>
       <div>
-        <p> Blockchain with a mining difficulty of {blockchain.difficulty} and a mining reward of {blockchain.miningReward}.</p>
-        <p>
-          Blocks :
-        </p>
-        <div>
-            {blockchain.chain.map((block, index) => (
-            <li key={index}>Block N°{index + 1} <DisplayOneBlock BlockToDisplay={block}/></li>))}
-         </div>
-        <p>
-          Achieved transactions :
-        </p>
-        <div>
-            {blockchain.transactions.map((transaction, index) => (
-            <li key={index}>Transaction N°{index + 1}
-              <DisplayTransaction TransactionToDisplay={transaction}/>
-            </li>))}
-         </div>
+        {blockchain.chain.map((block, index) => (
+          <li key={index}>Block N°{index + 1} <DisplayOneBlock BlockToDisplay={block} /></li>))}
       </div>
+      <p>
+        Achieved transactions :
+        </p>
+      <div>
+        {blockchain.transactions.map((transaction, index) => (
+          <li key={index}>Transaction N°{index + 1}
+            <DisplayTransaction TransactionToDisplay={transaction} />
+          </li>))}
+      </div>
+    </div>
   );
 };
 export default DisplayBlokChain;
