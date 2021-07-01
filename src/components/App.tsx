4107ce11f4b828c82fc8219f7f3c3b2df253e791
ipsonmoulinga <sallibrahima1992@gmail.com>
2021-07-01 03:31:11 +0200

    dead codes cleansed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,39 +1,6 @@
 import React, { ReactElement } from 'react';
-// import '../style/App.css';
-// import {
-//   BrowserRouter as Router, Route, Switch,
-// } from 'react-router-dom';
-// import {
-//   // Drawer,
-//   Grid, IconButton,
-//   // Button
-// } from '@material-ui/core';
-// // import {
-// //   HomeSideBarElement,
-// //   SettingsSideBarElement,
-// //   DashBoardSideBarElement,
-// //   SignUpSideBarElement,
-// //   SignInSideBarElement,
-// //   AboutSideBarElement,
-// //   BlockChainSideBarElement,
-// // } from './SideBarComponents';
-// import MenuIcon from '@material-ui/icons/Menu';
-// import BlockChain from './BlockChain';
-// // import SideBar, { ISideBarComponent } from './SideBar';
-// import About from './About';
-// import Settings from './Settings';
-// import Dashboard from './Dashboard';
-// import Home from './Home';
-// import SignIn from './SignIn';
-// import SignUp from './SignUp';
-// import ApppBar from './ApppBar';
-// // import ApppBar from './ApppBar';
-// import PersistentDrawerLeft from './PersistentDrawer';
-import PersistentDrawerLeft from './PersistentDrawer';
 
-// const MenuList:ISideBarComponent[] = [HomeSideBarElement,
-//   SettingsSideBarElement, DashBoardSideBarElement,
-//   SignUpSideBarElement, SignInSideBarElement, AboutSideBarElement, BlockChainSideBarElement];
+import PersistentDrawerLeft from './PersistentDrawer';
 
 function App() : ReactElement {
   return (
